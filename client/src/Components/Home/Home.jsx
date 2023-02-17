9000568cb7c20b7591dc43c01686b827910f2d7f
Franco Bosco <franbosco12@gmail.com>
2023-02-17 11:22:31 -0300

    front

---
+++ client/src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SideMenu from '../SideMenu/SideMenu';
+
+export default function Home() {
+	return (
+		<div>
+			<SideMenu />
+		</div>
+	);
+}
---
+++ client/src/Components/Landing/Landing.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Landing.css';
+import { Link } from 'react-router-dom';
+
+export default function Landing() {
+	return (
+		<div className="max-w-screen h-screen max-h-screen">
+			<div className="flex justify-center ">
+				<div className="flex flex-col ">
+					<div>
+						<h1 className="homeTittle">Gestor de cobranzas</h1>
+					</div>
+					<div className="flex justify-center mt-32">
+						<Link to="/home">
+							<button className="landingButton">Ingresa</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
