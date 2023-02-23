abb098e1e5dd5ff5bfca745a78b9ba0c53edad4a
Juan Manuel <juanberrazmontyn@hotmail.com>
2023-02-23 17:41:26 -0300

    ViewCategories

---
+++ client/src/Components/Categories/Categories.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import CategoriesTable from "../CategoriesTable";
+import SideMenu from "../SideMenu/SideMenu"
+
+export default function Categories () {
+    return (
+        <div className="flex max-w-screen bg-slate-200">
+			<div>
+				<SideMenu />
+			</div>
+			<div>
+				<div className="flex flex-col justify-center mt-8 ml-20 text-gray-700">
+					<h1 className="text-3xl underline mb-4">Categoria</h1>
+				</div>
+				<div className="flex mb-5">
+					<hr className="border-1 border-zinc-400 w-[75vw]" />
+				</div>
+				<button className="addClientButton">Agregar categoria</button>
+				<CategoriesTable />
+			</div>
+		</div>
+    )
+}
---
+++ client/src/Components/CategoriesTable/CategoriesTable.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+export default function ClientsTable() {
+	return (
+		<div className="ml-4">
+			<table className="w-[65vw]">
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Nombre de categoria</th>
+                        <th>Rubro Categoria</th>
+					</tr>
+				</thead>
+				<tbody></tbody>
+			</table>
+		</div>
+	);
+}
